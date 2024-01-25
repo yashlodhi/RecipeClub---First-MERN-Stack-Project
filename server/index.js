40cda364509f4c4a4cbfd111a8073fcb9f5f962a
YASH LODHI <71387667+yashlodhi@users.noreply.github.com>
2024-01-25 06:39:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ mongoose
 app.use(express.json({limit:"150mb"}));
 app.use(express.urlencoded({ extended: true, limit:"150mb"}));
 
-app.use( cors() ) ;
+app.use( cors({
+  origin: true,
+}) ) ;
 
 
 
