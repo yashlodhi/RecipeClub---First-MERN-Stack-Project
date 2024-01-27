6d64a6b6e632f14a413a3dbe41789d3fd8c3d8f3
YASH LODHI <71387667+yashlodhi@users.noreply.github.com>
2024-01-27 21:55:48 +0530

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,15 +5,45 @@ import Login from "./Login";
 import Signup from "./Signup";
 import Recipe  from "./Recipe";
 import chefImg from "../chef.png"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import RecipeByUser from "./RecipeByUser";
+import MenuImg from "../menu.png";
 
 function App() {
   const [loggedStatus, setLoggedStatus] = useState(false);
   const [username, setUsername] = useState('');
+
+  useEffect(()=> {
+    const Menu_Bar_Icon = document.querySelector('#Menu_Bar_Icon') ; 
+    const navbarOptions = document.querySelector('#Navbar_Options') ; 
+    let isMenuOpen = false ; 
+
+    Menu_Bar_Icon.addEventListener('click' , ()=>{
+      if(isMenuOpen){
+        Menu_Bar_Icon.classList.remove('Menu_Bar_Active') ; 
+        navbarOptions.classList.remove('Navbar_Options_Active') ; 
+        isMenuOpen = false ; 
+      }
+      else{
+        Menu_Bar_Icon.classList.add('Menu_Bar_Active') ; 
+        navbarOptions.classList.add('Navbar_Options_Active') ; 
+        isMenuOpen = true ; 
+      }
+    });
+
+    document.querySelectorAll('#Navbar_Options a').forEach((element)=>{
+        element.addEventListener('click' , ()=> {
+          Menu_Bar_Icon.classList.remove('Menu_Bar_Active') ; 
+          navbarOptions.classList.remove('Navbar_Options_Active') ; 
+          isMenuOpen = false ; 
+          window.scrollTo(0, 0);
+        });
+    });
+    
+  }, []);
   
-  
+
   return (
     <>
       <BrowserRouter>
@@ -23,20 +53,23 @@ function App() {
           </p>
 
           <div id="nav-right">
+            <div className="link"> <Link to="/">Home</Link> </div>
+            <div className="link"> <Link to="/allrecipes">All Recipes</Link> </div>
             <div className="link">
-              <Link to="/allrecipes">All Recipes</Link>
-            </div>
-            <div className="link">
-              {loggedStatus ? (
-                <Link to="/myprofile">My Profile</Link>
-              ) : (
-                <Link to="/login">Login</Link>
-                )}
-            </div>
+              { loggedStatus ? ( <Link to="/myprofile">My Profile</Link> ) : (<Link to="/login">Login</Link> ) } </div>
           </div>
+          <img src={MenuImg} id="Menu_Bar_Icon"/>  
         </div>
 
         <div className="main-section">
+
+          <div id="Navbar_Options">
+            <div> <Link to="/">Home</Link> </div>
+            <div> <Link to="/allrecipes">All Recipes</Link> </div>
+            <div> 
+            { loggedStatus ? (<Link to="/myprofile">My Profile</Link>) : (<Link to="/login">Login</Link>) } </div>
+          </div>
+
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/allrecipes" element={<AllRecipes />} />
